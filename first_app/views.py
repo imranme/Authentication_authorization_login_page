4926af142c017fc2639c81649b4efc7d06aef1b2
imranme <tusharimran914076@gmail.com>
2025-04-22 23:56:46 +0600

    first commit

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import .forms import RegistrForm
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == 'POST':
+        form = RegistrForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            print(form.cleaned_data)
+        else:
+            form = RegistrForm()
+        return render(request, './home.html'{'form': form})
+        
